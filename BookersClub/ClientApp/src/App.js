1bf0609f32408804356a83672517806bbdee5cc3
SujithAlla <sphur@DESKTOP-SGAOR3B>
2022-03-14 22:27:46 -0400

    Created Login Page

--- BookersClub/ClientApp/src/App.js
+++ BookersClub/ClientApp/src/App.js
@@ -5,7 +5,9 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
+
 import './custom.css'
+import { Login } from './components/LoginPage';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -16,6 +18,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Route path='/Login' component={Login} />
       </Layout>
     );
   }
---
+++ BookersClub/ClientApp/src/components/LoginPage.js
@@ -0,0 +1,32 @@
+﻿import React, { Component } from 'react';
+
+
+export class Login extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <p> Welcome to Bookers Club </p>
+                </div>
+                <div>
+                    <label >Username</label>
+                    <input className="User" type="text" />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input className="User"type="text" />
+                </div>
+                <div>
+                    <input type="submit" />
+                </div>
+            </div>
+        );
+
+    }
+
+
+
+
+}
